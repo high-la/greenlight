628d019ad5200f05f152d84327f3a53ab921a425
Haile <hailecst@gmail.com>
2026-01-27 18:49:59 +0300

    sending error messages, using helper methods

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,10 +11,15 @@ func (app *application) routes() http.Handler {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
-	// Register the relevant methods, URL patterns and handler function for our
-	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
-	// http.MethodPost are constants which equate to the string "GET" and "POST"
-	// respectively.
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
+	// it as the custom error handler for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	//
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
