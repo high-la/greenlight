429d4b737ad6cb694757f1c19629bf415fae0513
Haile <hailecst@gmail.com>
2026-02-04 09:55:43 +0300

    spliting up the middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -205,30 +205,34 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
-func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+// Create a new requireAuthenticatedUser() middleware to check that a user is not
+// anonymous.
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// Use the contextGetUser() helper that we made earlier to retrieve the user
-		// information from the request context
 		user := app.contextGetUser(r)
 
-		// Use the contextGetUser() helper that we made earlier to retrieve the user
-		// information from the request context
 		if user.IsAnonymous() {
 			app.authenticationRequiredResponse(w, r)
 			return
 		}
 
-		// If the user is not activated, use the inactiveAccountResponse() helper to
-		// inform them that they need to activate their account.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	// Rather than returning this http.HandlerFunc we assign it to the variable fn.
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+		// Check that a user is activated.
 		if !user.Activated {
 			app.inactiveAccountResponse(w, r)
 			return
 		}
-
-		// Call the next handler in the chain
 		next.ServeHTTP(w, r)
-
 	})
+	// Wrap fn with the requireAuthenticatedUser() middleware before returning it.
+	return app.requireAuthenticatedUser(fn)
 }
